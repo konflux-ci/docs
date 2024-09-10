7fd1c91128d27b60f6e7d300ef060d73ccf3e312
Ralph Bean <rbean@redhat.com>
2024-09-10 14:30:02 -0400

    Add the release-service-catalog repo

--- hack/generate-tekton-index.sh
+++ hack/generate-tekton-index.sh
@@ -4,10 +4,12 @@
 set -e -o pipefail
 
 VCS_URL="$1"
-SUBDIR="$2"
+BRANCH="$2"
+SUBDIR="$3"
 
-if [ -z "${VCS_URL+x}" ] || [ -z "${SUBDIR+x}" ]; then
-    echo "Usage: ./hack/generate-tekton-index.sh https://github.com/konflux-ci/build-definitions task"
+if [ -z "${VCS_URL+x}" ] || [ -z "${BRANCH+x}" ] || [ -z "${SUBDIR+x}" ]; then
+    echo "Usage::"
+    echo "  ./hack/generate-tekton-index.sh https://github.com/konflux-ci/build-definitions main task"
     exit 1
 fi
 
@@ -16,7 +18,7 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 WORKDIR=$(mktemp -d --suffix "-$(basename "${BASH_SOURCE[0]}" .sh)")
 NOW=$(date +%Y-%m-%d)
 
-git clone "$VCS_URL" "$WORKDIR/repo"
+git clone -b "$BRANCH" "$VCS_URL" "$WORKDIR/repo"
 
 cat << EOF > "$WORKDIR/tasks.adoc"
 EOF
@@ -24,10 +26,14 @@ EOF
 # Build tasks
 (
 cd "$WORKDIR/repo"
-find "$SUBDIR/"*/*/ -maxdepth 0 -type d | awk -F '/' '{ print $0, $2, $3 }' | \
+find "$SUBDIR/"*/ "$SUBDIR/"*/*/ -maxdepth 0 -type d | awk -F '/' '{ print $0, $2, $3 }' | \
 while read -r task_dir task_name task_version
 do
+    if [[ "${task_dir}" == */tests/ ]]; then
+        continue
+    fi
     if [ ! -f "${task_dir}/${task_name}.yaml" ]; then
+        echo "${task_dir}/${task_name}.yaml"
         echo "Skipping ${task_name}.yaml. kustomize rendering not supported yet"
         continue
     fi
@@ -39,10 +45,14 @@ do
         fi
     fi
 
-    url="${VCS_URL}/tree/main/${task_dir}"
+    if [ "${task_version}" == "" ]; then
+        task_version=$(yq '.metadata.labels."app.kubernetes.io/version" // "undefined"' "$task_dir/$task_name.yaml")
+    fi
+
+    url="${VCS_URL}/tree/${BRANCH}/${task_dir}"
 
     echo >> "$WORKDIR/tasks.adoc"
-    echo -n "[[$task_name-$task_version]]$task_name ($task_version):: " >> "$WORKDIR/tasks.adoc"
+    echo -n "[[$task_name]]$task_name ($task_version):: " >> "$WORKDIR/tasks.adoc"
 
     description=$(yq '.spec.description | split("\n") | .[0]' "$task_dir/$task_name.yaml")
     echo -n "$description " >> "$WORKDIR/tasks.adoc"
